6852f909a7e3a47857ad30d30465c2ce430e461f
Rafał Kiełbus <rav3n.pl@gmail.com>
2021-06-11 00:47:45 +0200

    Linter

--- contracts/SmartHoldETH.sol
+++ contracts/SmartHoldETH.sol
@@ -5,53 +5,58 @@ pragma solidity 0.8.4;
 import "./PriceFeedInterface.sol";
 
 contract SmartHoldETH {
-  address immutable public owner;
-  uint immutable public depositedAt;
-  uint immutable public lockForDays;
-  int immutable public minimumPrice;
-  PriceFeedInterface internal priceFeed;
+    address public immutable owner;
+    uint256 public immutable depositedAt;
+    uint256 public immutable lockForDays;
+    int256 public immutable minimumPrice;
+    PriceFeedInterface internal priceFeed;
 
-  modifier restricted() {
-    require(msg.sender == owner, "Access denied!");
-    _;
-  }
-
-  constructor(address _priceFeed, uint _lockForDays, int _minimumPrice) payable {
-    require(_lockForDays < 4000, "Too long lockup period!");
-    require(_minimumPrice >= 0, "Minimum price must not be negative!");
-    owner = msg.sender;
-    lockForDays = _lockForDays;
-    minimumPrice = _minimumPrice;
-    depositedAt = block.timestamp;
-    priceFeed = PriceFeedInterface(_priceFeed);
-  }
-
-  function withdraw() external restricted {
-    require(canWithdraw(), "Cannot withdraw yet!");
-    payable(owner).transfer(address(this).balance);
-  }
-
-  function canWithdraw() public view restricted returns (bool) {
-    bool timeCondition = (depositedAt + (lockForDays * 1 days)) < block.timestamp;
-    bool priceCondition = false;
-
-    if(timeCondition) {
-      return true;
+    modifier restricted() {
+        require(msg.sender == owner, "Access denied!");
+        _;
     }
 
-    if(minimumPrice == 0) {
-      priceCondition = false;
-    } else {
-      priceCondition = (getPrice() >= minimumPrice);
+    constructor(
+        address _priceFeed,
+        uint256 _lockForDays,
+        int256 _minimumPrice
+    ) payable {
+        require(_lockForDays < 4000, "Too long lockup period!");
+        require(_minimumPrice >= 0, "Minimum price must not be negative!");
+        owner = msg.sender;
+        lockForDays = _lockForDays;
+        minimumPrice = _minimumPrice;
+        depositedAt = block.timestamp;
+        priceFeed = PriceFeedInterface(_priceFeed);
     }
-    return priceCondition;
-  }
 
-  receive() external payable {}
+    function withdraw() external restricted {
+        require(canWithdraw(), "Cannot withdraw yet!");
+        payable(owner).transfer(address(this).balance);
+    }
 
-  function getPrice() public view returns (int) {
-    (, int price,,,) = priceFeed.latestRoundData();
+    function canWithdraw() public view restricted returns (bool) {
+        bool timeCondition =
+            (depositedAt + (lockForDays * 1 days)) < block.timestamp;
+        bool priceCondition = false;
 
-    return price / 10e7;
-  }
+        if (timeCondition) {
+            return true;
+        }
+
+        if (minimumPrice == 0) {
+            priceCondition = false;
+        } else {
+            priceCondition = (getPrice() >= minimumPrice);
+        }
+        return priceCondition;
+    }
+
+    receive() external payable {}
+
+    function getPrice() public view returns (int256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+
+        return price / 10e7;
+    }
 }
